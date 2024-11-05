c600b7dd8b0c56cc827c57ace5d15d929cf756e8
João Manoel <joaomanoel.ms.br@gmail.com>
2024-11-05 12:57:56 -0400

    fix error walletSlug to walletId

--- apps/web/src/app/dashboard/transactions/incomes/page.tsx
+++ apps/web/src/app/dashboard/transactions/incomes/page.tsx
@@ -1,28 +1,38 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getCategorys } from '@/http/get-categorys'
 import { getTransactions } from '@/http/get-transactions'
 import { getWallet } from '@/http/get-wallet'
 
 import { TransactionsTable } from '../transaction-table'
 
 export default async function IncomesPage() {
-  const walletSlug = cookies().get('wallet')?.value
+  const walletId = cookies().get('wallet')?.value
 
-  if (!walletSlug) {
+  if (!walletId) {
     redirect('/dashboard')
   }
 
   const transactions = await getTransactions({
     type: 'INCOME',
-    walletSlug,
+    walletId,
   })
 
-  const wallet = await getWallet(walletSlug)
+  const wallet = await getWallet(walletId)
+
+  const categorys = await getCategorys({
+    walletId,
+    type: 'INCOME',
+  })
 
   return (
     <div className="space-y-2">
-      <TransactionsTable data={transactions} wallet={wallet} />
+      <TransactionsTable
+        data={transactions}
+        wallet={wallet}
+        categorys={categorys}
+      />
     </div>
   )
 }
