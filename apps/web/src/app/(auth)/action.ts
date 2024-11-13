1f222ed1bd270e9f2d53f384f319dc79ff3c054b
João Manoel <joaomanoel.ms.br@gmail.com>
2024-11-13 14:58:43 -0400

    env added

--- apps/web/src/app/(auth)/action.ts
+++ apps/web/src/app/(auth)/action.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { env } from '@mb/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGoogleAction() {
@@ -7,13 +8,10 @@ export async function signInWithGoogleAction() {
     'https://accounts.google.com/o/oauth2/v2/auth',
   )
 
-  googleSignInURL.searchParams.set(
-    'client_id',
-    '185187910671-iajgjvvctddl16hrf9udmk7dde0k309l.apps.googleusercontent.com',
-  )
+  googleSignInURL.searchParams.set('client_id', env.GOOGLE_OAUTH_CLIENT_ID)
   googleSignInURL.searchParams.set(
     'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
+    env.GOOGLE_OAUTH_REDIRECT_URI,
   )
   googleSignInURL.searchParams.set('response_type', 'code')
   googleSignInURL.searchParams.set('scope', 'openid profile email')
