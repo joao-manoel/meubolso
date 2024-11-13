54405467234f4fc1678f73d1dced4259702620c3
João Manoel <joaomanoel.ms.br@gmail.com>
2024-11-13 15:52:56 -0400

    removed optional

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 export const env = createEnv({
   server: {
     JWT_SECRET: z.string(),
-    DATABASE_URL: z.string().optional(),
+    DATABASE_URL: z.string(),
     PORT: z.coerce.number().default(3333),
 
     GOOGLE_OAUTH_CLIENT_SECRET: z.string(),
